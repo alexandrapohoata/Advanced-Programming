376d2fe5b97d64bb6a396f379e1378585b51b543
alexandrapohoata <79059482+alexandrapohoata@users.noreply.github.com>
2021-02-22 18:52:24 +0200

    Modificare variabile din main

--- Lab2/Compulosory/src/Main.java
+++ Lab2/Compulosory/src/Main.java
@@ -1,36 +1,36 @@
 public class Main {
-    private static int total_sources = 3;
-    private static int total_destinations = 3;
+    private static int TOTAL_SOURCES = 3;
+    private static int TOTAL_DESTINATIONS = 3;
     int[] demand = new int[]{20, 25, 25};
-    int[] suply = new int[]{10, 35, 25};
+    int[] supply = new int[]{10, 35, 25};
 
     public static void main(String[] args) {
         /*
          * Fiecare sursa contine o lista de destinatii
          * pentru fiecare destinatie a unei surse, precizez costul
          */
-        Source[] source = new Source[total_sources];
-        Destination[] destination = new Destination[total_destinations];
+        Source[] sources = new Source[TOTAL_SOURCES];
+        Destination[] destinations = new Destination[TOTAL_DESTINATIONS];
 
-        destination[0] = new Destination("D1", 2);
-        destination[1] = new Destination("D2", 3);
-        destination[2] = new Destination("D3", 1);
-        source[0] = new Source("S1", SourceType.FACTORY, destination);
+        destinations[0] = new Destination("D1", 2);
+        destinations[1] = new Destination("D2", 3);
+        destinations[2] = new Destination("D3", 1);
+        sources[0] = new Source("S1", SourceType.FACTORY, destinations);
 
-        destination[0] = new Destination("D1", 5);
-        destination[1] = new Destination("D2", 4);
-        destination[2] = new Destination("D3", 8);
-        source[1] = new Source("S2", SourceType.WAREHOUSE, destination);
+        destinations[0] = new Destination("D1", 5);
+        destinations[1] = new Destination("D2", 4);
+        destinations[2] = new Destination("D3", 8);
+        sources[1] = new Source("S2", SourceType.WAREHOUSE, destinations);
 
-        destination[0] = new Destination("D1", 5);
-        destination[1] = new Destination("D2", 6);
-        destination[2] = new Destination("D3", 8);
-        source[2] = new Source("S3", SourceType.WAREHOUSE, destination);
+        destinations[0] = new Destination("D1", 5);
+        destinations[1] = new Destination("D2", 6);
+        destinations[2] = new Destination("D3", 8);
+        sources[2] = new Source("S3", SourceType.WAREHOUSE, destinations);
 
         /*
          * o instanta a problemei contine o lista de surse si o lista de destinatii
          */
-        Problem problem = new Problem(source, destination);
+        Problem problem = new Problem(sources, destinations);
         System.out.println(problem);
     }
 }
